f53c9a172b2a0708d31677f6ccfef399ad78ac3f
Malith Senaweera <malithsenaweera@gmail.com>
2016-02-16 09:12:33 +0530

    Integrate Server with Markov script to generate responses

--- markov.py
+++ markov.py
@@ -5,118 +5,92 @@ import random
 import sys
 import codecs
 
-tempMapping = {}
+class Markov:
+    def __init__(self):
+        self.tempMapping = {}
+        self.mapping = {}
+        self.starts = []
+        filename = 'all.txt'
+        self.markovLength = 1
+        self.buildMapping(self.wordlist(filename), self.markovLength)
 
-mapping = {}
+    def toHashKey(self, lst):
+        return tuple(lst)
 
-starts = []
+    def wordlist(self, filename):
+        f = codecs.open(filename,"r","utf-16")
+        wordlist = [w for w in re.findall(r"[^\x00-\x7F']+|[.,!?;]", f.read())]
+        f.close()
+        return wordlist
 
-def toHashKey(lst):
-    return tuple(lst)
-
-def wordlist(filename):
-    f = codecs.open(filename,"r","utf-16")
-    wordlist = [w for w in re.findall(r"[^\x00-\x7F']+|[.,!?;]", f.read())]
-    f.close()
-    return wordlist
-
-def addItemToTempMapping(history, word):
-    global tempMapping
-    while len(history) > 0:
-        first = toHashKey(history)
-        if first in tempMapping:
-            if word in tempMapping[first]:
-                tempMapping[first][word] += 1.0
+    def addItemToTempMapping(self, history, word):
+        while len(history) > 0:
+            first = self.toHashKey(history)
+            if first in self.tempMapping:
+                if word in self.tempMapping[first]:
+                    self.tempMapping[first][word] += 1.0
+                else:
+                    self.tempMapping[first][word] = 1.0
             else:
-                tempMapping[first][word] = 1.0
-        else:
-            tempMapping[first] = {}
-            tempMapping[first][word] = 1.0
-        history = history[1:]
+                self.tempMapping[first] = {}
+                self.tempMapping[first][word] = 1.0
+            history = history[1:]
 
-# Building and normalizing the mapping.
-def buildMapping(wordlist, markovLength):
-    global tempMapping
-    starts.append(wordlist [0])
-    for i in range(1, len(wordlist) - 1):
-        if i <= markovLength:
-            history = wordlist[: i + 1]
-        else:
-            history = wordlist[i - markovLength + 1 : i + 1]
-        follow = wordlist[i + 1]
-        # if the last elt was a period, add the next word to the start list
-        if history[-1] == "." and follow not in ".,!?;":
-            starts.append(follow)
-        addItemToTempMapping(history, follow)
-    # Normalize the values in tempMapping, put them into mapping
-    for first, followset in tempMapping.iteritems():
-        total = sum(followset.values())
-        mapping[first] = dict([(k, v / total) for k, v in followset.iteritems()])
+    # Building and normalizing the mapping.
+    def buildMapping(self, wordlist, markovLength):
+        self.starts.append(wordlist [0])
+        for i in range(1, len(wordlist) - 1):
+            if i <= markovLength:
+                history = wordlist[: i + 1]
+            else:
+                history = wordlist[i - markovLength + 1 : i + 1]
+            follow = wordlist[i + 1]
+            # if the last elt was a period, add the next word to the start list
+            if history[-1] == "." and follow not in ".,!?;":
+                self.starts.append(follow)
+            self.addItemToTempMapping(history, follow)
+        # Normalize the values in tempMapping, put them into mapping
+        for first, followset in self.tempMapping.iteritems():
+            total = sum(followset.values())
+            self.mapping[first] = dict([(k, v / total) for k, v in followset.iteritems()])
 
-def next(prevList):
-    sum = 0.0
-    retval = ""
-    index = random.random()
-    # Shorten prevList until it's in mapping
-    while toHashKey(prevList) not in mapping:
-        prevList.pop(0)
-    # Get a random word from the mapping, given prevList
-    for k, v in mapping[toHashKey(prevList)].iteritems():
-        sum += v
-        if sum >= index and retval == "":
-            retval = k
-    return retval
-
-def genSentence(markovLength, seed = None):
-    # Start with a random "starting word"
-    if not seed:
-        curr = random.choice(starts)
-    else:
-        curr = seed.split()
-        curr = curr[0].decode('utf-8')
-
-    sent = curr.capitalize()
-    prevList = [curr]
-    # Keep adding words until we hit a period
-    while (curr not in "."):
-        # print 'prevList', prevList
-        curr = next(prevList)
-        prevList.append(curr)
-        # if the prevList has gotten too long, trim it
-        if len(prevList) > markovLength:
+    def next(self, prevList):
+        sum = 0.0
+        retval = ""
+        index = random.random()
+        # Shorten prevList until it's in mapping
+        while self.toHashKey(prevList) not in self.mapping:
             prevList.pop(0)
-        if (curr not in ".,!?;"):
-            sent += " " # Add spaces between words (but not punctuation)
-        sent += curr
-    return sent
+        # Get a random word from the mapping, given prevList
+        for k, v in self.mapping[self.toHashKey(prevList)].iteritems():
+            sum += v
+            if sum >= index and retval == "":
+                retval = k
+        return retval
 
-def main():
-    if len(sys.argv) < 2:
-        sys.stderr.write('Usage: ' + sys.argv [0] + ' text_source [chain_length=1] [sinhala=None]\n')
-        sys.exit(1)
+    def genSentence(self, markovLength, seed = None):
+        # Start with a random "starting word"
+        if not seed:
+            curr = random.choice(self.starts)
+        else:
+            curr = seed.split()
+            curr = curr[0]
 
-    filename = sys.argv[1]
-    markovLength = 1
-    if len (sys.argv) == 3:
-        markovLength = int(sys.argv [2])
+        sent = curr.capitalize()
+        prevList = [curr]
+        # Keep adding words until we hit a period
+        while (curr not in "."):
+            # print 'prevList', prevList
+            curr = self.next(prevList)
+            prevList.append(curr)
+            # if the prevList has gotten too long, trim it
+            if len(prevList) > markovLength:
+                prevList.pop(0)
+            if (curr not in ".,!?;"):
+                sent += " " # Add spaces between words (but not punctuation)
+            sent += curr
+        return sent
 
-    buildMapping(wordlist(filename), markovLength)
-
-    while True:
-        seed = raw_input('> ')
-        res = genSentence(markovLength, seed)
-        print (res)
-        print '\n'
-        f.write(seed.decode('utf-8'))
-        f.write('> ' + res)
-        f.write('\n')
-        
-        
-
-if __name__ == "__main__":
-    try:
-        f = codecs.open("res.txt", "w", "utf-16")
-        main()
-    except KeyboardInterrupt:
-        print ("Bye bye")
-        f.close() 
+    def main(self, seed):
+        res = self.genSentence(self.markovLength, seed)
+        return res
--- server.py
+++ server.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask import request
 from flask import render_template
+import markov
 
 app = Flask(__name__)
 messages = []
+
+mark = markov.Markov()
 
 @app.route("/")
 def main():
@@ -13,7 +16,10 @@ def main():
 def process():
     text = request.form['text']
     messages.append(text)
+    response = mark.main(text)
+    messages.append(response)
     return render_template("index.html", data=messages)
 
 if __name__ == "__main__":
+    app.debug = True
     app.run()
